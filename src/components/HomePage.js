061a86ba1805a606c45bba95eb6223a004bd2634
Levon Arakelyan <levonarakelian@live.com>
2017-11-27 01:40:18 +0400

    update home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,73 +19,26 @@ const HomePage = () => {
 
 class levon extends human {
 
-   static profession = null
-   static religion = 'christian'
-   static beliefs = [ ${ <Glitcher amount={3}>loading...</Glitcher> } ]
-   static values = [ cannot load... ]
-   static principles = [ 'dependencies' ]
-   static secrets = [ ]
-   static quotes = [
-      'never say never',
-      'be the change you want to see in the world'
-   ]
-   static friends = [ ${ <input type='text' /> } ]
+   profession = null
+   fortes = ['quick study']
 
-   personality
-   skills
-   work
-   hobbies
-   contacts
-   static thoughts = [
-      'on form & essence',
-      'on asking the right questions',
-      'on the power of belief',
-      'on government innovation',
-      'on convergence of disciplines'
-   ]
+   ${ <Link to='/interests'>interests()</Link> }
 
-   static social = {
+   work = [ ${ <a href='http://mojilytics.com' target='_blank'>mojilytics</a> }, "iUNU" ]
+
+   social = {
       facebook: '${ <a href='http://facebook.com/levonarakelyan' target='_blank'>levonarakelyan</a> }',
       twitter: '${ <a href='http://twitter.com/levonarakelian' target='_blank'>@levonarakelian</a> }'
    }
 
-   static contact = {
-      email: ✉️ => '${ <a href='mailto:me@levon.me'>me@levon.me</a> }',
-      phone: 📱 => '${ <a href='tel:374-95-773-700'>+374 95 77-37-00</a>}'
+   contact = {
+      ✉️ => '${ <a href='mailto:me@levon.me'>me@levon.me</a> }',
+      📱 => '${ <a href='tel:374-95-773-700'>+374 95 77-37-00</a>}'
    }
-
-   static getCurrentMood = () => { ... }
-
-   ${ <Link to='/interests'>interests()</Link> }
-
-   static personality = {${ <Link to='/languages'>🤔 😎 🤠 😇 🤑 👻</Link> }}
-   static settings = {${ <Link to='/languages'>...</Link> }}
 }
 
 export default levon
 `}
-
-{/* {`
-// welcome!
-class levon extends human {
-   mood = "😆"
-   settings = {
-      openminded: true,
-      compassion: "●●●●●●●●●●",
-      respectfulness: "●●●●●●●●●○",
-   }
-   values = [ ]
-   interests = { }
-   startups = [ "pushloop", "boomerang", "iUNU" ]
-   knowledge = {
-      programming: [ "react.js", "meteor", ...stuff ]
-   }
-   languages = () => {`}<Link to='/languages'>...languages</Link>{`}
-   twitter = () => `}<a href='//twitter.com/levonarakelian' target='_blank'>@levonarakelian</a>{`
-   facebook = () => `}<a href='//facebook.com/levonarakelyan' target='_blank'>levonarakelyan</a>{`
-   `}<a href='//facebook.com/levonarakelyan' target='_blank'>play()</a>{`
-}
-`} */}
       </CodeWrapper>
    )
 }
---
+++ src/components/HomePage_ideas.js
@@ -0,0 +1,91 @@
+import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
+
+import CodeWrapper from './CodeWrapper'
+import synter from '../utils/synter'
+
+import SyntaxHighlighter from '../utils/SyntaxHighlighter'
+import Glitcher from '../utils/Glitcher'
+import FullScreenGlitch from '../utils/FullScreenGlitch'
+
+const HomePage = () => {
+   return (
+      <CodeWrapper>
+         {/* <FullScreenGlitch /> */}
+{synter`
+/**
+ * welcome to levon's personal space!
+ */
+
+class levon extends human {
+
+   static profession = null
+   static religion = 'christian'
+   static beliefs = [ ${ <Glitcher amount={3}>loading...</Glitcher> } ]
+   static values = [ cannot load... ]
+   static principles = [ 'dependencies' ]
+   static secrets = [ ]
+   static quotes = [
+      'never say never',
+      'be the change you want to see in the world'
+   ]
+   static friends = [ ${ <input type='text' /> } ]
+
+   personality
+   skills
+   work
+   hobbies
+   contacts
+   static thoughts = [
+      'on form & essence',
+      'on asking the right questions',
+      'on the power of belief',
+      'on government innovation',
+      'on convergence of disciplines'
+   ]
+
+   static social = {
+      facebook: '${ <a href='http://facebook.com/levonarakelyan' target='_blank'>levonarakelyan</a> }',
+      twitter: '${ <a href='http://twitter.com/levonarakelian' target='_blank'>@levonarakelian</a> }'
+   }
+
+   static contact = {
+      ✉️ => '${ <a href='mailto:me@levon.me'>me@levon.me</a> }',
+      📱 => '${ <a href='tel:374-95-773-700'>+374 95 77-37-00</a>}'
+   }
+
+   ${ <Link to='/interests'>interests()</Link> }
+
+   static personality = {${ <Link to='/languages'>🤔 😎 🤠 😇 🤑 👻</Link> }}
+   static settings = {${ <Link to='/languages'>...</Link> }}
+}
+
+export default levon
+`}
+
+{/* {`
+// welcome!
+class levon extends human {
+   mood = "😆"
+   settings = {
+      openminded: true,
+      compassion: "●●●●●●●●●●",
+      respectfulness: "●●●●●●●●●○",
+   }
+   values = [ ]
+   interests = { }
+   startups = [ "pushloop", "boomerang", "iUNU" ]
+   knowledge = {
+      programming: [ "react.js", "meteor", ...stuff ]
+   }
+   languages = () => {`}<Link to='/languages'>...languages</Link>{`}
+   twitter = () => `}<a href='//twitter.com/levonarakelian' target='_blank'>@levonarakelian</a>{`
+   facebook = () => `}<a href='//facebook.com/levonarakelyan' target='_blank'>levonarakelyan</a>{`
+   `}<a href='//facebook.com/levonarakelyan' target='_blank'>play()</a>{`
+}
+`} */}
+      </CodeWrapper>
+   )
+}
+
+export default HomePage
